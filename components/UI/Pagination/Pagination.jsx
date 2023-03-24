48cea10e27ea45987c2fe88e207eea3cc560685d
Олег Бовкуш <obovkush@gmail.com>
2023-03-24 15:02:18 +1000

    Добавлена пагинация

---
+++ components/UI/Pagination/Pagination.jsx
@@ -0,0 +1,45 @@
+import Button from "../Button/Button";
+import Input from "../Input/Input";
+import cl from "./Pagination.module.css";
+
+const Pagination = ({ totalPages, currentPage, onPageChange }) => {
+	const handlePrevClick = () => {
+		onPageChange(currentPage - 1);
+	};
+
+	const handleNextClick = () => {
+		onPageChange(currentPage + 1);
+	};
+
+	return (
+		<div className={cl.pagination}>
+			<Button
+				variant={currentPage !== 1 && "primary"}
+				size="small"
+				disabled={currentPage === 1}
+				onClick={handlePrevClick}
+                text={"<"}
+			/>
+			<div>
+				<Input
+					type="number"
+					min="1"
+					max={totalPages}
+                    className={cl.pageInput}
+					value={currentPage}
+					onChange={(e) => (Number(e.target.value) <= totalPages) && onPageChange(Number(e.target.value))}
+				/>
+                {' '} / {totalPages}
+			</div>
+			<Button
+				variant={currentPage !== totalPages && "primary"}
+				size="small"
+				disabled={currentPage === totalPages}
+				onClick={handleNextClick}
+                text={">"}
+			/>
+		</div>
+	);
+};
+
+export default Pagination;
